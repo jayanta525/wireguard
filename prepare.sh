43a2413b71eadc0df26512d8c85ad6e6efb54f91
Jayantajit Gogoi <30702133+jayanta525@users.noreply.github.com>
2020-03-30 23:28:56 +0530

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -7,4 +7,14 @@ sudo apt-get install wireguard -y
 sudo systemctl disable systemd-resolved.service
 sudo systemctl stop systemd-resolved
 
+rm -f /etc/resolv.conf
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
+ufw allow ssh
+ufw allow 53
+echo "y" | ufw enable
+
+mkdir wg0
+cd wg0
+umask 077
+wg genkey | tee privatekey | wg pubkey > publickey
