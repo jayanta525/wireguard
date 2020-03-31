5ca45ac322f0b7ceec406e07f5d3b8169b7f1a80
Jayantajit Gogoi <30702133+jayanta525@users.noreply.github.com>
2020-03-31 09:15:27 +0530

    Update 1-prepare.sh

--- 1-prepare.sh
+++ 1-prepare.sh
@@ -11,6 +11,11 @@ sudo systemctl stop systemd-resolved
 rm -f /etc/resolv.conf
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
+echo "net.ipv4.ip_forward = 1
+net.ipv6.conf.all.forwarding = 1" > /etc/sysctl.d/wg.conf
+
+sysctl --system
+
 ufw allow ssh
 ufw allow 53
 echo "y" | ufw enable
@@ -19,8 +24,10 @@ mkdir wg0
 cd wg0
 umask 077
 wg genkey | tee privatekey | wg pubkey > publickey
-privk=$(cat privatekey)
 echo "[Interface]" > wg0.conf
 echo "PrivateKey = $(cat privatekey)" >> wg0.conf
 echo "Address = 10.42.0.1/24" >> wg0.conf
 echo "ListenPort = 53" >> wg0.conf
+echo "PostUp = iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE; ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE" >> wg0.conf
+echo "PostDown = iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE; ip6tables -t nat -D POSTROUTING -o eth0 -j MASQUERADE" >> wg0.conf
+
