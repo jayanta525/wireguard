b3fee1dd7d436d9a44ef25cf644d8d14dd39835b
Jayantajit Gogoi <30702133+jayanta525@users.noreply.github.com>
2020-03-31 08:58:33 +0530

    Update and rename prepare.sh to 1-prepare.sh

--- 1-prepare.sh
+++ 1-prepare.sh
@@ -3,6 +3,7 @@
 sudo add-apt-repository ppa:wireguard/wireguard -y
 sudo apt-get update
 sudo apt-get install wireguard -y
+sudo apt-get install qrencode -y
 
 sudo systemctl disable systemd-resolved.service
 sudo systemctl stop systemd-resolved
@@ -18,3 +19,8 @@ mkdir wg0
 cd wg0
 umask 077
 wg genkey | tee privatekey | wg pubkey > publickey
+privk=$(cat privatekey)
+echo "[Interface]" > wg0.conf
+echo "PrivateKey = $(cat privatekey)" >> wg0.conf
+echo "Address = 10.42.0.1/24" >> wg0.conf
+echo "ListenPort = 53" >> wg0.conf
