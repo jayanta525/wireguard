2845b86a0c3483479f4650185071003b8f2f0475
Jayantajit Gogoi <30702133+jayanta525@users.noreply.github.com>
2020-03-31 20:28:20 +0530

    Create a-setup.sh

---
+++ a-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "You need to run this script as root"
+    exit 1
+fi
+
+sudo add-apt-repository ppa:wireguard/wireguard -y
+sudo apt-get update
+sudo apt-get install wireguard -y
+sudo apt-get install qrencode -y
+
+sudo systemctl disable systemd-resolved.service
+sudo systemctl stop systemd-resolved
+rm -f /etc/resolv.conf
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
+ufw allow ssh
+ufw allow 53
+echo "y" | ufw enable
+
+
